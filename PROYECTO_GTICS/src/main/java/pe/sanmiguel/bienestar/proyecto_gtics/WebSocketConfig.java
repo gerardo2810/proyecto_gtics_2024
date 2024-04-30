59e5066c33a33d1165f854e40ac5eaa882bfba87
Pierohc <133154904+Pierohc@users.noreply.github.com>
2024-04-30 14:26:49 -0500

    chat

--- PROYECTO_GTICS/src/main/java/pe/sanmiguel/bienestar/proyecto_gtics/WebSocketConfig.java
+++ PROYECTO_GTICS/src/main/java/pe/sanmiguel/bienestar/proyecto_gtics/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package pe.sanmiguel.bienestar.proyecto_gtics;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -10,9 +11,11 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer{
 
+    @Autowired
+    private ChatHandler chatHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-
+        registry.addHandler(chatHandler,"/chat");
     }
 }
