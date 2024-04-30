f7d3741ec288b0f99ff812d0ba184232bd93ea3e
Pierohc <133154904+Pierohc@users.noreply.github.com>
2024-04-30 17:41:40 -0500

    aaa

--- PROYECTO_GTICS/src/main/java/pe/sanmiguel/bienestar/proyecto_gtics/WebSocketConfig.java
+++ PROYECTO_GTICS/src/main/java/pe/sanmiguel/bienestar/proyecto_gtics/WebSocketConfig.java
@@ -16,7 +16,7 @@ public class WebSocketConfig implements WebSocketConfigurer{
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatHandler,"/chat");
+        registry.addHandler(chatHandler, "/chat").setAllowedOrigins("*");
     }
 }
 
